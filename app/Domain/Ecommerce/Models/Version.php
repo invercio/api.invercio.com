6c814bb23bf8be457abe87fe7c48b286a2f5236a
Dany Gagnon <danygagnon@Danys-MacBook-Air.local>
2023-01-20 20:13:38 -0500

    feat: add relation for version

--- app/Domain/Ecommerce/Models/Version.php
+++ app/Domain/Ecommerce/Models/Version.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Ecommerce\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Version extends Model
@@ -14,5 +15,10 @@ class Version extends Model
     public function variations(): BelongsToMany
     {
         return $this->belongsToMany(Variation::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
     }
 }
